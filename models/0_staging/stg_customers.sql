24ae7129b42abb2fa992760565262e8efd981669
Pat Bates <pat.bates@dbtlabs.com>
2026-08-18 12:54:11 +0000

    removed temp col

--- models/0_staging/stg_customers.sql
+++ models/0_staging/stg_customers.sql
@@ -2,6 +2,5 @@ select
     cast(id as bigint) as customer_id,
     trim(first_name) as first_name,
     trim(last_name) as last_name,
-    concat_ws(' ', trim(first_name), trim(last_name)) as full_name,
-    'temp' as temp
+    concat_ws(' ', trim(first_name), trim(last_name)) as full_name
 from {{ source('jaffle_shop', 'customers') }}
